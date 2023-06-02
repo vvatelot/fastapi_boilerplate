663a071aeb3a62da67ca9cf152f9c41345e729b0
Vincent Vatelot <vincent.vatelot@ext.adeo.com>
2023-06-02 16:44:16 +0200

    ✨ feat(models): Add auto registration for models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,7 @@
-from app.models.tasks import *  # noqa: F403
+import os
+
+__all__ = [
+    f.replace(".py", "")
+    for f in os.listdir(os.path.dirname(__file__))
+    if f.endswith(".py") and not f.startswith("__")
+]
